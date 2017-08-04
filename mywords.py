08fc1d926f1b4e9339a36caceb22421fa5cbf1e4
Sebas <sebasheston@gmail.com>
2017-08-04 00:22:45 -0700

    Changed the imports to make the lines shorter

--- mywords.py
+++ mywords.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import secrets
-import hashlib
+from secrets import randbelow
+from secerets import SystemRandom
+#import hashlib
 
 
 word_dict = {}
-
 SPECIAL_CHARACTERS = '`~!@#$%^&*()-_+=+?/><,'
 
 with open('eff_large_wordlist.txt','r') as words_list:
@@ -16,20 +16,22 @@ text = ''
 for x in range(7):
     num = 0    
     while num < 11111:
-        num = secrets.randbelow(66666)
+        num = randbelow(66666)
+        #Since the words list is a dice we can't have any numbers 0789
         for digit in "0789":
             if digit in str(num):
                 num = 0 
 
 
-    text = text+word_dict[str(num)]
+    passw = passw+word_dict[str(num)]
    
-    rand1 = secrets.randbelow(99999999999999999)
-    rand2 = secrets.randbelow(99999999999999999)
-    print ('{}, {}'.format(rand1, rand2))
+    rand1 = SystemRandom 
+    rand2 = SystemRandom
+    #print ('{}, {}'.format(rand1, rand2))
+    
     if rand2 < rand1:
         
-       text = text+SPECIAL_CHARACTERS[secrets.randbelow(len(SPECIAL_CHARACTERS))]
+       passw = passw + SPECIAL_CHARACTERS[randbelow(len(SPECIAL_CHARACTERS))]
 
 
 
